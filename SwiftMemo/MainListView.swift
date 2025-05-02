780be6b4f312889aad47239dc9d18c49e3d8f261
chocopai2 <wjh2311@naver.com>
2025-05-02 20:45:32 +0900

    step1

---
+++ SwiftMemo/MainListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainListView.swift
+//  SwiftMemo
+//
+//  Created by wangjun on 5/2/25.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainListView()
+}
